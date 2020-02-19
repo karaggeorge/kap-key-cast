39eb364ce86884a41f4770558ba91adb7aca4efa
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-02-19 11:15:33 -0500

    Add titles and descriptions to options

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const hasPermissions = require('macos-accessibility-permissions');
 
 const config = {
 	size: {
+		title: 'Size',
+		description: 'Size of the UI.',
 		type: 'string',
 		enum: ['Small', 'Normal', 'Large'],
 		default: 'Normal',
 		required: true
 	},
 	delay: {
+		title: 'Delay',
+		description: 'How long the UI stays on screen.',
 		type: 'number',
 		minimum: 0,
 		default: 0.5,
 		required: true
 	},
 	keyCombinationsOnly: {
+		title: 'Key Combinations Only',
+		description: 'When enabled, the UI will only show for combinations with accelerators.',
 		type: 'boolean',
 		default: true
 	},
 	centerInCropper: {
+		title: 'Center in cropper',
+		description: 'When enabled, the UI will use the cropper area to position. Otherwise it will use the whole screen.',
 		type: 'boolean',
 		default: true
 	}
